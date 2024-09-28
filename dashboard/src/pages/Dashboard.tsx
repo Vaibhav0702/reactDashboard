ad176f09cca1b0f40451203ead7de02deb8e6377
Vaibhav <vaibhavginnalwar@gmail.com>
2024-09-28 15:19:04 +0530

    add interface for dashboard component

--- dashboard/src/pages/Dashboard.tsx
+++ dashboard/src/pages/Dashboard.tsx
@@ -5,63 +5,66 @@ import embryoBank from "../Assets/SVG/EmbryoBank.svg"
 import addUser from "../Assets/SVG/user-add.svg";
 import clock from "../Assets/SVG/clock.svg"
 
+
+interface DashboardCardData {
+  title: string;
+  subtitle: string;
+  value: string;
+  icon: string;
+}
+
+const dashboardData: DashboardCardData[] = [
+  {
+    title: "Total Patients",
+    subtitle: "1000",
+    value: "1000",
+    icon: embryoBank,
+  },
+  {
+    title: "Total Embryo Graded",
+    subtitle: "3000",
+    value: "3000",
+    icon: embryoBank,
+  },
+  {
+    title: "Avg #Emb Graded/Patient",
+    subtitle: "06",
+    value: "06",
+    icon: addUser,
+  },
+  {
+    title: "Avg Turnaround Time",
+    subtitle: "123 Hrs",
+    value: "123 Hrs",
+    icon: clock,
+  },
+];
+
+
 export default function Dashboard() {
   return (
     <div className='bg-light px-2'>
 
       <div className="row">
-        <div className="col-lg-3 col-md-6 p-1">
-          <div className="card m-0 p-1">
-            <div className="card-body d-flex gap-2 p-1">
-              <div>
-                <img className='dashBoardIcon d-flex align-items-center' src={embryoBank} alt="embryoBank" />
-              </div>
-              <div>
-                <h5 className="card-title dashboardbH">1000</h5>
-                <p className="card-text dashboardSubH">Total Patients</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 p-1">
-          <div className="card m-0 p-1">
-            <div className="card-body d-flex gap-2 p-1">
-              <div>
-                <img className='dashBoardIcon d-flex align-items-center' src={embryoBank} alt="embryoBank" />
-              </div>
-              <div>
-                <h5 className="card-title dashboardbH">3000</h5>
-                <p className="card-text dashboardSubH">Total Embryo Graded</p>
+        {dashboardData.map((card, index) => (
+          <div key={index} className="col-lg-3 col-md-6 p-1">
+            <div className="card m-0 p-1">
+              <div className="card-body d-flex gap-2 p-1">
+                <div>
+                  <img
+                    className="dashBoardIcon d-flex align-items-center"
+                    src={card.icon}
+                    alt={card.title}
+                  />
+                </div>
+                <div className="flex-grow-1">
+                  <h5 className="card-title dashboardbH">{card.value}</h5>
+                  <p className="card-text dashboardSubH">{card.title}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="col-lg-3 col-md-6 p-1">
-          <div className="card m-0 p-1">
-            <div className="card-body d-flex gap-2 p-1">
-              <div>
-                <img className='dashBoardIcon d-flex align-items-center' src={addUser} alt="addUser" />
-              </div>
-              <div className='flex-grow-1'>
-                <h5 className="card-title dashboardbH">06</h5>
-                <p className="card-text dashboardSubH">Avg #Emb Graded/Patient</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 p-1">
-          <div className="card m-0 p-1">
-            <div className="card-body d-flex gap-2 p-1">
-              <div>
-                <img className='dashBoardIcon d-flex align-items-center' src={clock} alt="clock" />
-              </div>
-              <div>
-                <h5 className="card-title dashboardbH">123 Hrs</h5>
-                <p className="card-text dashboardSubH">Avg Turnaround Time</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
 
       
